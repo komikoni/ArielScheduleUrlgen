80801a44b671bf4bee65e461281f4bb0b5e1c2d4
keisuke konishi <konishi.keisuke@tis.co.jp>
2017-02-28 22:43:01 +0900

    init commit

---
+++ bookmarklet.js
@@ -0,0 +1,10 @@
+javascript:void((function(undefined) {
+  var tool_id ="ariel_schedule_urlgen";
+  var script = document.createElement('script');
+  script.src = '//example.com/bundle.js';
+  script.id  = tool_id;
+  script.setAttribute('data-protocol','http');
+  script.setAttribute('data-hostname','example.com');
+  script.setAttribute('data-systemname','Ariel Air One Enterprise');
+  document.body.appendChild(script);
+})());
---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+// gulpfile.js
+var gulp       = require('gulp');
+var browserify = require('browserify');
+var source     = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+
+// browserify and gulp.
+gulp.task('watch', function () {
+    gulp.watch(['./*.js'], ['build-js']);
+});
+gulp.task('browserify', function () {
+    return browserify('./main.js')
+            .bundle()
+            .pipe(source('bundle.js'))
+            .pipe(gulp.dest('./dist/'));
+});
+gulp.task('build-js', function () {
+    return browserify({
+        entries: './main.js' // どのファイルからビルドするか
+    }).plugin('licensify') // licensifyプラグインの有効化
+        .bundle() // browserifyの実行
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(uglify({
+            preserveComments: 'license' // ライセンスコメントを残しつつminify
+        }))
+        .pipe(gulp.dest('./dist')); // 出力
+});
