82377f7655b686f62331c6625914f065a9f1dabb
komikoni <komikoni@gmail.com>
2017-03-30 23:51:42 +0900

    word-break:break-all;

--- main.js
+++ main.js
@@ -214,7 +214,7 @@ info_html += '<div><input type="checkbox" id="organizer_flag" /><label for="orga
 info_html += '<h2 style="margin-bottom:15px;font-size:20px">出力内容(JSON)</h2>';
 info_html += '<textarea id="paramJson" rows="10" cols="100" spellcheck="false"></textarea><br />';
 info_html += '<h2 style="margin-bottom:15px;font-size:20px">出力内容(LongURL)</h2>';
-info_html += '<textarea id="longUrl" rows="6" cols="100" spellcheck="false"></textarea><button id="copyLongUrl">コピー</button><button id="openLongUrl" >オープン</button><br />';
+info_html += '<textarea id="longUrl" rows="6" cols="100" spellcheck="false" style="word-break:break-all;"></textarea><button id="copyLongUrl">コピー</button><button id="openLongUrl" >オープン</button><br />';
 info_html += '<h2 style="margin-bottom:15px;font-size:20px">Google Shortener URL</h2>';
 info_html += '<button id="generateShortUrl" >短縮URL生成</button><br />';
 info_html += '<input type="text" id="shortUrl" /><button id="copyShortUrl">コピー</button><button id="openShortUrl" >オープン</button><br />';
