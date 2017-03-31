f284230bf334ee565055fac7e1cc7ff4f86ad27d
komikoni <komikoni@gmail.com>
2017-03-31 09:28:50 +0900

    try catch

--- main.js
+++ main.js
@@ -216,7 +216,7 @@ info_html += '<h2 style="margin-bottom:10px;font-size:20px">出力内容JSONとU
 info_html += '<textarea id="paramJson" rows="6" cols="100" spellcheck="false"></textarea><br />';
 info_html += '<textarea id="longUrl" rows="6" cols="100" spellcheck="false" style="word-break:break-all;"></textarea><button id="copyLongUrl">URLをコピー</button><button id="openLongUrl" >URLを開く</button><br />';
 info_html += '<h2 style="margin-bottom:10px;font-size:20px">Google Shortener URL</h2>';
-info_html += '<button id="generateShortUrl" >短縮URL生成</button> (api-key必須)<br />';
+info_html += '<button id="generateShortUrl" >短縮URL生成</button> (api-key必須) ';
 info_html += '<input type="text" id="shortUrl" readonly="readonly" size="20" /><button id="copyShortUrl">URLをコピー</button><button id="openShortUrl" >URLを開く</button><br />';
 $info.append(info_html);
 
@@ -234,7 +234,13 @@ $('#organizer_flag, #attendee_flag, #facility_flag').on('change', function() {
     $('#longUrl').val(generateArielUrl(param_map2));
 });
 $('#paramJson').on('keyup', function() {
-    $('#longUrl').val(generateArielUrl(JSON.parse($('#paramJson').val())));
+    var url;
+    try {
+        url = generateArielUrl(JSON.parse($('#paramJson').val()));
+    } catch (error) {
+        url = "json error";
+    }
+    $('#longUrl').val(url);
 });
 $('#copyLongUrl').on('click', function() {
     clipboadCopy('longUrl');
